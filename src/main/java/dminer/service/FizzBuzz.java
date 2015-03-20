07d6196bedd28c4130d191c75936ae9379cdd977
Prathan D <zyracuze@gmail.com>
2015-03-20 13:34:41 +0700

    Change logic on Buzz rule

--- src/main/java/dminer/service/FizzBuzz.java
+++ src/main/java/dminer/service/FizzBuzz.java
@@ -7,7 +7,7 @@ public class FizzBuzz {
 			return "Fizz";
 		if(number % 5 == 0)
 			return "Buzz";
-		if(number % 3 == 0 && number % 5 == 0)
+		if(number % 3 == 0 && number % 5 == 1)
 			return "FizzBuzz";
 		return String.valueOf(number);
 	}
