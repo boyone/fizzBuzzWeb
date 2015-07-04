88ed3d33d6aada3fd499147367387c58fe290c46
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2015-07-04 16:12:58 +0700

    Add fizzbuzz testcase

--- src/test/java/dminer/service/FizzBuzzTest.java
+++ src/test/java/dminer/service/FizzBuzzTest.java
@@ -25,7 +25,8 @@ public class FizzBuzzTest {
             {3, "Fizz", "countThreeShouldSayFizz"},
             {4, "4", "countFourShouldSayFour"},
             {5, "Buzz", "countFiveShouldSayBuzz"},
-            {6, "Fizz", "countSixShouldSayFizz"}
+            {6, "Fizz", "countSixShouldSayFizz"},
+            {15, "FizzBuzz", "countFifteenShouldSayFizzBuzz"}
         });
     }
 
