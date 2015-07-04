a3c516b1f5ce9b52e983dc19865323dcb170473f
Thawatchai Jongsuwanpaisan <boyone.g@gmail.com>
2015-07-04 13:31:18 +0700

    Remove fizzbuzz rule.

--- src/main/java/dminer/service/FizzBuzz.java
+++ src/main/java/dminer/service/FizzBuzz.java
@@ -3,8 +3,6 @@ package dminer.service;
 public class FizzBuzz {
 
 	public String say(int number) {
-		if(number % 3 == 0 && number % 5 == 0)
-			return "FizzBuzz";
 		if(number % 3 == 0)
 			return "Fizz";
 		if(number % 5 == 0)
